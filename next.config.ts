7c6b6b1878a8c0165964a33bb68ae1fa3a7376e1
Carter Dobbs <dgi6@txstate.edu>
2025-04-25 00:51:48 -0500

    Build site

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  basePath: '/LLMAttackSim',
 };
 
 export default nextConfig;
