61b47e9da3fa50afc481446e2f2228fe0dfb4dd5
Carter Dobbs <dgi6@txstate.edu>
2025-04-25 01:26:19 -0500

    Set up github pages workflow

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
 import type { NextConfig } from 'next';
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   /* config options here */
+  assetPrefix: isProduction ? '/LLMAttackSim' : '',
+  basePath: isProduction ? '/LLMAttackSim' : '',
   output: 'export',
-  basePath: '/LLMAttackSim',
 };
 
 export default nextConfig;
